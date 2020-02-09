cc0db73f6f02638f8aeff2d24317cf7668e12cbb
Raphael Magalhães <raphael.sdm@gmail.com>
2020-02-09 20:47:51 -0300

    Add Jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    preset: 'jest-expo',
+    transformIgnorePatterns: [
+        'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base)'
+    ],
+    collectCoverage: false,
+    collectCoverageFrom: [
+        '**/*.{js,jsx,ts,tsx}',
+        '!**/coverage/**',
+        '!**/node_modules/**',
+        '!**/web-build/**',
+        '!**/babel.config.js',
+        '!**/jest.config.js'
+    ]
+}
